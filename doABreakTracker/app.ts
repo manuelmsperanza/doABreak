9cbebc1fc56d340bbbaac2d3594800260256c78d
manuelmsperanza <manu.speranz@gmail.com>
2022-10-14 15:33:32 +0200

    dbTracker first commit

--- doABreakTracker/app.ts
+++ doABreakTracker/app.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import * as fs from 'fs';
+
+
+var content = {
+    action: (process.argv.slice(2)[0]),
+    timestamp: new Date()
+}
+
+fs.writeFileSync(process.env.HOME + '\\doABreak.json', JSON.stringify(content));
